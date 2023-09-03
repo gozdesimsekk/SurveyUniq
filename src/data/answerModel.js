9247c8d7d4edbabf7b3d8d4dca1c218abed6cb13
Irmak Ege <irmakege@Irmaks-MacBook-Air.local>
2023-09-03 14:45:51 +0300

    data models added

---
+++ src/data/answerModel.js
@@ -0,0 +1,36 @@
+const response = [
+    {
+        "ResponseID": 1, //INT //PRIMARY KEY
+        "RespondentID": 1, //INT //FOREIGN KEY
+        "SurveyID": 1, //INT //FOREIGN KEY
+        "StartDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "EndDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+    },
+
+]
+
+// for people whom not tested bu UNIQGENE that has no swabcode
+const nonuniqrespondent = [
+    {
+        "RespondentID": 1, //INT //PRIMARY KEY
+        "Name": "Irmak Ege", //VARCHAR(100)
+        "Email": "irmak@uniqgene.com", //VARCHAR(100)
+    }
+]
+
+// for people whom tested bu UNIQGENE that has swabcode
+const uniqrespondent = [
+    {
+        "RespondentID": 1, //INT //PRIMARY KEY
+        "Code": "123456", //VARCHAR(100) //Swabcodes
+    },
+]
+
+const answer = [
+    {
+        "AnswerID": 1, //INT //PRIMARY KEY
+        "ResponseID": 1, //INT //FOREIGN KEY
+        "QuestionID": 1, //INT //FOREIGN KEY
+        "Answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at.", //TEXT
+    },
+]
---
+++ src/data/questionModel.js
@@ -0,0 +1,95 @@
+const surveyStatus = 
+[
+    {
+    "SurveyStatusID": 1, //INT //PRIMARY KEY
+    "SurveyStatus": "Open" //VARCHAR(100)
+    },
+    {
+    "SurveyStatusID": 2, //INT //PRIMARY KEY
+    "SurveyStatus": "Closed" //VARCHAR(100)
+    },
+    {
+    "SurveyStatusID": 3, //INT //PRIMARY KEY
+    "SurveyStatus": "Planned" //VARCHAR(100)
+    },
+    {
+    "SurveyStatusID": 4, //INT //PRIMARY KEY
+    "SurveyStatus": "Suspended" //VARCHAR(100)
+    }
+]
+
+const surveyInfo = 
+[
+    {
+        "SurveyID": 1, //INT //PRIMARY KEY
+        "SurveyStatusID": 1, //INT //FOREIGN KEY
+        "Name": "Nutrition Demand Survey", //VARCHAR(100)
+        "Description": "Collecting demand to nutrition package", //TEXT
+        "StartDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "EndDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "MinResponses": "100", //INT
+        "MaxResponses": "500" //INT
+    },
+    {
+        "SurveyID": 2, //INT //PRIMARY KEY
+        "SurveyStatusID": 2, //INT //FOREIGN KEY
+        "Name": "Report Interpretation", //VARCHAR(100)
+        "Description": "Satisfaction from Interpretation Session", //TEXT
+        "StartDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "EndDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "MinResponses": "100", //INT
+        "MaxResponses": "500" //INT
+    },
+    {
+        "SurveyID": 3, //INT //PRIMARY KEY
+        "SurveyStatusID": 3, //INT //FOREIGN KEY
+        "Name": "User Confounding Variables", //VARCHAR(100)
+        "Description": "Collect user data", //TEXT
+        "StartDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "EndDate": "YYYY-MM-DD HH:MI:SS", //DATETIME
+        "MinResponses": "100", //INT
+        "MaxResponses": "500" //INT
+    }
+]
+
+const questionType = 
+[
+    {
+        "QuestionTypeID": 1, //INT //PRIMARY KEY
+        "QuestionType": "Open", //VARCHAR(100)
+    },
+    {
+        "QuestionTypeID": 2, //INT //PRIMARY KEY
+        "QuestionType": "Dropdown", //VARCHAR(100)
+    },
+    {
+        "QuestionTypeID": 3, //INT //PRIMARY KEY
+        "QuestionType": "Multiple Choice", //VARCHAR(100)
+    },
+    {
+        "QuestionTypeID": 4, //INT //PRIMARY KEY
+        "QuestionType": "Boolean", //VARCHAR(100)
+    },
+]
+
+const question = 
+[
+    {
+        "QuestionID": 1, //INT //PRIMARY KEY
+        "SurveyID": 2, //INT //FOREIGN KEY
+        "QuestionTypeID": 2, //INT //FOREIGN KEY
+        "Order": 1, //INT
+        "QuestionText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at.", //VARCHAR(2000)
+        "IsMandatory": "True" //BOOLEAN
+    },
+    {
+        "QuestionID": 2, //INT
+        "SurveyID": 2, //INT //FOREIGN KEY
+        "QuestionTypeID": 2, //INT //FOREIGN KEY
+        "Order": 2, //INT
+        "QuestionText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at.", //VARCHAR(2000)
+        "IsMandatory": "False" //BOOLEAN
+    }
+]
+
+
